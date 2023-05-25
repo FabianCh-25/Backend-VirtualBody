902ac97e29e38600bfa7ff2e8f6d3c40e367c068
unknown <Proyectos@H52-002.upc.edu.pe>
2023-05-25 11:19:53 -0500

    correcciones Aula

--- src/main/java/com/g6/virtualbody/config/SwaggerConfig.java
+++ src/main/java/com/g6/virtualbody/config/SwaggerConfig.java
@@ -14,9 +14,9 @@ public class SwaggerConfig {
 
     @Bean
     public Docket apiDocket(){
-        return new Docket(DocumentationType.SPRING_WEB.SWAGGER_2)
+        return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.g6.virtualbody.controllers"))
+                .apis(RequestHandlerSelectors.basePackage("com.go.virtualbody.controllers"))
                 .paths(PathSelectors.any())
                 .build();
     }
--- src/main/java/com/g6/virtualbody/controllers/AulaController.java
+++ src/main/java/com/g6/virtualbody/controllers/AulaController.java
@@ -1,9 +1,32 @@
 package com.g6.virtualbody.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.g6.virtualbody.dtos.AulaDTO;
+import com.g6.virtualbody.entities.Aula;
+import com.g6.virtualbody.services.IAulaService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/aulas")
+@RequestMapping("/Aulas")
 public class AulaController {
+    @Autowired
+    private IAulaService aS;
+
+    @PostMapping
+    public void insert(@RequestBody AulaDTO dto){
+        ModelMapper m=new ModelMapper();
+        Aula a=m.map(dto,Aula.class);
+        aS.insert(a);
+    }
+    @GetMapping
+    public List<AulaDTO> list(){
+        return aS.list().stream().map(x->{
+            ModelMapper m=new ModelMapper();
+            return m.map(x,AulaDTO.class);
+        }).collect(Collectors.toList());
+    }
 }
