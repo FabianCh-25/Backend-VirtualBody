7d90f9b2c1f3559b0943d70f00321f0d975819d1
tu nombre de usuario <u20201f777@upc.edu.pe>
2023-05-25 01:17:08 -0500

    Creacion Aula (pt2) faltan correcciones

---
+++ src/main/java/com/g6/virtualbody/controllers/AulaController.java
@@ -0,0 +1,9 @@
+package com.g6.virtualbody.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/aulas")
+public class AulaController {
+}
