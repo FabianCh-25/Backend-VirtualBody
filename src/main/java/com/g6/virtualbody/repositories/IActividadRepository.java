6a35cfc45b3539d3d66c36aee03ea91b6c4ca425
tu nombre de usuario <u20201f777@upc.edu.pe>
2023-05-31 13:05:07 -0500

    Eliminación de Actividades en Aula

--- src/main/java/com/g6/virtualbody/repositories/IActividadRepository.java
+++
@@ -1,9 +0,0 @@
-package com.g6.virtualbody.repositories;
-
-import com.g6.virtualbody.entities.Actividad;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IActividadRepository extends JpaRepository<Actividad,Integer> {
-}
