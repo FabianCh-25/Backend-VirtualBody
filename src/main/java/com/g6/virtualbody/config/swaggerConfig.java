c434301ea2535c9177d0c60ccaec9d9f54f27e07
RonaldCM <u201920185@upc.edu.pe>
2023-06-12 19:06:24 -0500

    Agregue los mios

--- src/main/java/com/g6/virtualbody/config/swaggerConfig.java
+++ src/main/java/com/g6/virtualbody/config/swaggerConfig.java
@@ -10,8 +10,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class swaggerConfig {
-
+public class  swaggerConfig {
     @Bean
 public Docket apiDocket(){
 
